d74b686f9d11810f0a99123113953edb9f41546f
PoojaD12345 <dhuripooja01@gmail.com>
2023-01-14 23:29:28 +0530

    filter ui completed

--- src/components/LeftSideBar/LeftSidebar.jsx
+++ src/components/LeftSideBar/LeftSidebar.jsx
@@ -1,8 +1,74 @@
 import React from 'react'
+import style from './leftsidebar.module.css'
 
 const LeftSidebar = () => {
   return (
-    <div>LeftSidebar</div>
+    <div className={style.filter_main_box}>
+
+        {/* colours */}
+        <div>
+            <h3>Colours</h3>
+            <div>
+                <input type="checkbox" />
+                <h5>Red</h5>
+            </div>
+            <div>
+                <input type="checkbox" />
+                <h5>Blue</h5>
+            </div>
+            <div>
+                <input type="checkbox" />
+                <h5>Green</h5>
+            </div>
+       
+
+        {/* gender */}
+        
+            <h3>Gender</h3>
+            <div>
+                <input type="checkbox" />
+                <h5>Male</h5>
+            </div>
+            <div>
+                <input type="checkbox" />
+                <h5>Female</h5>
+            </div>
+        
+
+        {/*price  */}
+       
+            <h3>Price</h3>
+            <div>
+                <input type="checkbox" />
+                <h5>0-250</h5>
+            </div>
+            <div>
+                <input type="checkbox" />
+                <h5>251-450</h5>
+            </div>
+            <div>
+                <input type="checkbox" />
+                <h5>450</h5>
+            </div>
+       
+
+        {/* type */}
+        
+            <h3>Type</h3>
+            <div>
+                <input type="checkbox" />
+                <h5>Polo</h5>
+            </div>
+            <div>
+                <input type="checkbox" />
+                <h5>Hoodie</h5>
+            </div>
+            <div>
+                <input type="checkbox" />
+                <h5>Basic</h5>
+            </div>
+        </div>
+    </div>
   )
 }
 
